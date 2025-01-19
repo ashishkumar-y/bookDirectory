be2a451aed390b8a5b3247481443f40bf1ade25b
Ashish Kumar <ashishkumar@Sumits-MacBook-Air.local>
2025-01-19 16:55:25 +0530

    add feature

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+
+
+console.log(process.env.MONGO_URI);
 const express = require('express')
 const app = express()
 const { databaseConnection } = require('./connection/db')
@@ -13,17 +16,29 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 //--------routes----------------->
-app.use('/', routes)
+app.use('/api', routes)
 
 
 
 //---------server setup-------------->
 
 const PORT = process.env.PORT || 3004;
-app.listen(PORT, (err) => {
-    if (err) {
-        console.log(`server failed: ${err}`);
-    } else {
-        console.log(`server started at port:${PORT}`);
-    }
-})
+
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server started locally at port:${PORT}`);
+    });
+}
+
+module.exports = (req, res) => {
+    app(req, res);  // Vercel uses this to forward requests to the Express app
+};
+
+
+// app.listen(PORT, (err) => {
+//     if (err) {
+//         console.log(`server failed: ${err}`);
+//     } else {
+//         console.log(`server started at port:${PORT}`);
+//     }
+// })
